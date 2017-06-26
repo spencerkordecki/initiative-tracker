64945a8b4837d52fbce3b0625406b10f43f44bb8
Spencer Kordecki <spencerkordecki@gmail.com>
2017-06-25 20:00:52 -0400

    basic input functionality

--- app/components/InputRow.js
+++ app/components/InputRow.js
@@ -4,31 +4,74 @@ require('bulma/css/bulma.css');
 require('../index.css');
 
 class InputRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'initiative' : '',
+            'character-name': '',
+            'health': ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        var target = event.target;
+        var targetName = target.name;
+        var targetValue = target.value;
+
+        this.setState({
+            [targetName]: targetValue
+        });
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+    }
+
     render() {
         return <tr>
             <td>
                 <div className="field">
-                    <p class="control">
-                        <input className="input" type="number"></input>
+                    <p className="control">
+                        <input 
+                            name="initiative" 
+                            className="input" 
+                            type="number" 
+                            onChange={this.handleInputChange}>
+                        </input>
                     </p>
                 </div>
             </td>
             <td>
                 <div className="field">
-                    <p class="control">
-                        <input className="input" type="text"></input>
+                    <p className="control">
+                        <input 
+                            name="character-name" 
+                            className="input" 
+                            type="text" 
+                            onChange={this.handleInputChange}>
+                        </input>
                     </p>
                 </div>
             </td>
             <td>
                 <div className="field">
-                    <p class="control">
-                        <input className="input" type="number"></input>
+                    <p className="control">
+                        <input 
+                            name="health" 
+                            className="input" 
+                            type="number" 
+                            onChange={this.handleInputChange}>
+                        </input>
                     </p>
                 </div>
             </td>
             <td>
-                <a className={classNames("button", "is-primary")}>Add</a>
+                <a 
+                    className={classNames("button", "is-primary")}
+                    onClick={this.handleSubmit}>Add</a>
             </td>
         </tr>
     }
